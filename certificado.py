d4417a1fcc6eca2533346fbf9a79ee8dd6c4fe66
AndyMunico <andy_munico@storagedata.com.pe>
2026-08-11 10:47:53 -0500

    modificacion 2

--- certificado.py
+++ certificado.py
@@ -3,22 +3,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.common.exceptions import TimeoutException
 
 import os
 import re
 import time
 import easyocr
 
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get("https://www.empleosperu.gob.pe/portal-mtpe/#/login")
+OCR_READER = easyocr.Reader(['en'], gpu=False, verbose=False)
 
 ruta_del_codigo = os.path.dirname(os.path.abspath(__file__))
 carpeta = os.path.join(ruta_del_codigo, "imagenes_captcha")
 os.makedirs(carpeta, exist_ok=True)
 
-OCR_READER = easyocr.Reader(['en'], gpu=False, verbose=False)
+def crear_driver():
+
+    options = webdriver.ChromeOptions() # Agregado recientemente
+    prefs = {
+            "download.default_directory": r"C:\\Users\\Andy M\\Documents\\optimizacion\\CUL_V2\\CUL-scraper\\CULs",  # carpeta deseada
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "plugins.always_open_pdf_externally": True,  # descarga en lugar de abrir
+        }
+    options.add_experimental_option("prefs", prefs)
+
+
+    driver = webdriver.Chrome(options=options) # Agregado recientemente
+    driver.maximize_window()
+    driver.get("https://www.empleosperu.gob.pe/portal-mtpe/#/login")
+
+    return driver
 
 def login(driver, dni, contrasena):
     # # Esperamos a que el campo de DNI esté presente
@@ -82,7 +96,9 @@ def bienvenida_2(driver):
 
     time.sleep(5)  # Esperamos un segundo antes de continuar
 
+
 def obtener_certificado(driver):
+
     btn_certi = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(.,'Obtener Certificado')]")))
     btn_certi.click()
     time.sleep(5)  # Esperamos un segundo antes de continuar
@@ -96,7 +112,7 @@ def descargar_certificado(driver):
     # Esperamos a que el botón de descarga esté presente
     link_guardar = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, "//a[contains(., 'Guardar') and contains(@href, 'DescargarCUL')]"))
-)
+    )
     link_guardar.click()
     time.sleep(5)  # Esperamos un segundo antes de continuar
 
@@ -112,32 +128,84 @@ def ver_certificados(driver):
     btn_carpe.click()
     time.sleep(5)  # Esperamos un segundo antes de continuar
 
+def pasar_cul_descarga(driver):
+    try:
+        btn_generar = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.ID, "solicitar"))
+        )
+        time.sleep(3)
+        btn_generar.click()
+
+        print("Se hizo clic en 'Generar Certificado Único Laboral'")
+
+    except TimeoutException:
+
+        print("Botón 'Generar' no disponible. Usando historial...")
+
+        # btn_historial = WebDriverWait(driver, 10).until(
+        #     EC.element_to_be_clickable((By.XPATH, "//a[contains(., 'Ver mi carpeta de certificados')]"))
+        # )
+        # time.sleep(3)
+        # btn_historial.click()
+        carpeta_certificados(driver)
+        ver_certificados(driver)
+
+def cerrar_sesion(driver):
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[contains(@href, 'Salir.html') and contains(., 'Cerrar sesión')]"))
+    ).click()
+    
+    # WebDriverWait(driver, 10).until(
+    #     EC.presence_of_element_located((By.NAME, "dni"))
+    # )
+
+def finalizar_usuario(driver):
+    try:
+        cerrar_sesion(driver)
+    except:
+        pass
+    finally:
+        driver.quit()
+
 def main():
 
-    validacion = True
+    usuarios = [
+        ("48508068", "Porlasara1@"),
+        ("71251100", "zmaxwtf123"), #ENZO
+        ("72378883", "Fortuito147=") # DANIEL
+    ]
 
-    while validacion:
-        driver.refresh()
-        login(driver, "48508068", "Porlasara1@")
-        ruta = solve_captcha(driver)
-        texto = leer_captcha(ruta)
-        time.sleep(2)  # Esperamos un segundo antes de ingresar el captcha
-        ingresar_captcha(driver, texto)
-        alerta = tipo_alerta(driver)
+    for dni, contrasena in usuarios:
 
-        if alerta == "¡Bienvenido!":
-            validacion = False
+        validacion = True
 
-    bienvenida(driver)
-    bienvenida_2(driver)
-    obtener_certificado(driver)
+        #crear_driver()
+        driver = crear_driver()
 
-    #VALIDACION DE BOTON DE solicitar certificado
+        while validacion:
+            
+            driver.refresh()
+            ## AUTOMATIZAR
+            login(driver, dni, contrasena)
+            ruta = solve_captcha(driver)
+            texto = leer_captcha(ruta)
+            time.sleep(2)  # Esperamos un segundo antes de ingresar el captcha
+            ingresar_captcha(driver, texto)
+            alerta = tipo_alerta(driver)
 
-    #solicitar_certificado(driver)
-    carpeta_certificados(driver)
-    ver_certificados(driver)
-    descargar_certificado(driver)
+            if alerta == "¡Bienvenido!":
+                validacion = False
+
+        bienvenida(driver)
+        bienvenida_2(driver)
+        obtener_certificado(driver)
+
+        pasar_cul_descarga(driver)
+
+        descargar_certificado(driver)
+
+        finalizar_usuario(driver)
 
 if __name__ == "__main__":
     main()
